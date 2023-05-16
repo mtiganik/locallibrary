01352911c67a63166d31987900e69a673455e954
Mihkel Tiganik <mtiganik@gmail.com>
2023-05-16 14:28:41 +0300

    login functionality

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
+from .models import Book, Author, BookInstance, Genre
 from django.shortcuts import render
 from django.views import generic
 from django.shortcuts import get_object_or_404
-from .models import Book, Author, BookInstance, Genre
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     num_books = Book.objects.all().count()
@@ -11,12 +13,14 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
     num_authors = Author.objects.count()
-
+    num_visits = request.session.get('num_visits',0)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-        'num_authors': num_authors
+        'num_authors': num_authors,
+        'num_visits':num_visits
     }
 
     return render(request, 'index.html', context=context)
@@ -27,10 +31,12 @@ class BookListView(generic.ListView):
     paginate_by = 3
 
 
+
 class AuthorListView(generic.ListView):
     model = Author
 
-class AuthorDetailView(generic.DetailView):
+# @login_required
+class AuthorDetailView(LoginRequiredMixin, generic.DetailView):
     model = Author
 
 class BookDetailView(generic.DetailView):
